ac1c9fbfb3208fbcc7a97575f1cc4c5d4b9321d4
Даниил <hackermcyt@gmail.com>
2026-02-17 12:09:36 +0200

    123

--- modules/scripts/module3.cpp
+++ modules/scripts/module3.cpp
@@ -1,15 +1,59 @@
 #include <iostream>
+#include <cmath>
+#include <iomanip>
+
 #include "../../format.h"
 #include "../Modules.h"
-
 
 static void module_init()
 {
 	setupEnv();
 
+	std::cout << "=== Завдання 15 (Task 15) ===\n";
+
+	double a, b, c, xStart, xEnd, h;
+
+	std::cout << "Введіть a, b, c, Xпоч, Xкін, H: ";
+	std::cin >> a >> b >> c >> xStart >> xEnd >> h;
+
+	int Ac = (int)a;
+	int Bc = (int)b;
+	int Cc = (int)c;
+
+	// Умова: НЕ (A | B | C). Операція ~ це побітове НЕ.
+	bool showReal = (~(Ac | Bc | Cc)) != 0;
+
+	std::cout << "\nРезультат (" << (showReal ? "Дійсний" : "Цілий") << "):\n";
+	std::cout << "------------------------\n";
+	std::cout << "|    X     |     F     |\n";
+	std::cout << "------------------------\n";
+
+	for (double x = xStart; x <= xEnd + 0.00001; x += h) {
+		double F;
+
+		// Формули Завдання 15
+		if (x < 0) {
+			F = -a * x * x + b;
+		} else if (x > 0 && b != 0) {
+			F = x / (x - c);
+		} else {
+			F = x / -c;
+		}
+
+		std::cout << "| " << std::setw(8) << x << " | ";
+
+		if (showReal) {
+			std::cout << std::setw(9) << std::fixed << std::setprecision(2) << F << " |\n";
+		} else {
+			std::cout << std::setw(9) << (int)F << " |\n";
+		}
+	}
+	std::cout << "------------------------\n";
 }
+
+
 
 static Module module(
 	module_init,
-	""
+	"Завдання 15"
 );
